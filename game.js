c3ea9621415c4b3392db0bd5f13ec56273aa8387
Nathaniel LaPier <nathaniel.lapier@gmail.com>
2017-01-09 17:45:34 -0500

    fixing typos

--- game.js
+++ game.js
@@ -3,8 +3,12 @@ var vocab = require("./vocab")
 var game = {
 	wordList: vocab,
 	//These two methods work together to randomly select a word.
-	randomWordMachine: function(){return this.wordList[this.randomInt(0, this.wordList.length)]},
-	randomInt: function(min, max){return Math.floor(Math.random() * (max - min + 1)) + min}
+	randomWordMachine: function(){
+		return this.wordList[this.randomInt(0, this.wordList.length)]
+	},
+	randomInt: function(min, max){
+		return Math.floor(Math.random() * (max - min + 1)) + min
+	}
 }
 
 module.exports = game;
